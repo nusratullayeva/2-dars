dda5a7bc110bde6c4df872879f03ad419393954e
nusratllayeva <anvarsaidmurotov@gmail.com>
2023-02-27 08:31:02 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const userage = prompt('nechanci yiklda tugilgansiz');
+
+if (userage >= 1990) {
+    console.log('siz 21-asrda tugilgansiz');
+} else {
+    let yearsleft = 2000 - userage;
+    console.log('siz 20-arsda tugilgansz');
+}
+
+
+
+
+const yourName = prompt('qayerga bormoqchisiz');
+
+if (yourName === 'yevropa') {
+    console.log('siz yevropaga samolyot b-n borasz');
+} else {
+    console.log('Notugri malumot');
+}
+
+
+
+
+
+
+const myJob = prompt('narx tanlang');
+
+if (myJob === '$200') {
+    console.log('cipta narxi $200');
+} else {
+    console.log('sumka b-n $600');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const sourName = prompt('tulovlarni qildingizmi');
+
+if (sourName === 'ha') {
+    console.log('unda sizga oq yul tiliman');
+} else {
+    console.log('Notugri malumot');
+}
